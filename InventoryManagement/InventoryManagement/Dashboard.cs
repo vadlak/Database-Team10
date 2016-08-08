f298c53a8c260332bb6a46ac7615abcc49a4091a
Peyton Casper <peytoncas@gmail.com>
2016-08-08 17:29:56 -0500

    Initial Commit
    Added the base project and the SQL Database also with an example to
    connect and execute queries using the wrapper class.

---
+++ InventoryManagement/InventoryManagement/Dashboard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace InventoryManagement
+{
+    public partial class Dashboard : Form
+    {
+        SqlManager SQLManager;
+
+        public Dashboard()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            SQLManager = new SqlManager(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vadla\Documents\GitHub\Database-Team10\InventoryManagement\InventoryManagement\InventoryDB.mdf;Integrated Security=True");
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM Users";
+            cmd.CommandType = CommandType.Text;
+
+            SQLManager.Open();
+            SqlDataReader temp = SQLManager.ExecuteQuery(cmd);
+            SQLManager.Close();
+
+            SQLManager.Open();
+            SqlDataReader temp1 = SQLManager.ExecuteQuery("SELECT * FROM Users");
+            SQLManager.Close();
+
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement/SqlManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace InventoryManagement
+{
+    public class SqlManager
+    {
+        private string ConnectionString;
+        private SqlConnection connection;
+
+
+        public SqlManager(string ConnectionString)
+        {
+            this.ConnectionString = ConnectionString;
+            CreateConnection();
+        }
+
+        private bool CreateConnection()
+        {
+            try
+            {
+                connection = new SqlConnection(ConnectionString);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error Opening DB Connection " + e.ToString());
+            }
+            return false;
+        }
+        public void Open()
+        {
+            connection.Open();
+        }
+        public void Close()
+        {
+            connection.Close();
+        }
+        public SqlDataReader ExecuteQuery(string query)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandText = query;
+            command.CommandType = System.Data.CommandType.Text;
+            command.Connection = connection;
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            return reader;
+        }
+        public SqlDataReader ExecuteQuery(SqlCommand command)
+        {
+            command.Connection = connection;
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            return reader;
+        }
+    }
+}
